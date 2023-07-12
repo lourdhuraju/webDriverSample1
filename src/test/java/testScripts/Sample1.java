9465672ee42cb30c87040509965f7df82f2c8592
Administrator <Administrator@172.31.24.215>
2023-07-12 06:23:30 +0000

    Initial Project

---
+++ src/test/java/testScripts/Sample1.java
@@ -0,0 +1,16 @@
+package testScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java/4.10.0");
+		
+
+	}
+
+}

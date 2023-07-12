f99f355d0148789f6c73be3f3b79240b9a5ffb0e
Administrator <Administrator@172.31.24.215>
2023-07-12 06:39:18 +0000

    added token cred

--- src/test/java/testScripts/Sample1.java
+++ src/test/java/testScripts/Sample1.java
@@ -7,6 +7,7 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//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
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java/4.10.0");
 		
